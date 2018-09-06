14bdb773f2e516e5b545282de3d8d4b383a9a89e
Daniel Yoder <dan@pandastrike.com>
2018-09-06 13:19:34 -0700

    rm build artifacts from repo

--- build/npm/test/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// If the file exists to require,
-// the test passes :)
-require("../src/index");
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmNvZmZlZSJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7O0FBR0EsUUFBQSxjQUFBIiwic291cmNlc0NvbnRlbnQiOlsiIyBJZiB0aGUgZmlsZSBleGlzdHMgdG8gcmVxdWlyZSxcbiMgdGhlIHRlc3QgcGFzc2VzIDopXG5cbnJlcXVpcmUgXCIuLi9zcmMvaW5kZXhcIlxuIl0sInNvdXJjZVJvb3QiOiIifQ==
-//# sourceURL=index.coffee
